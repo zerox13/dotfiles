6355ed12a3a4d055dd9b2a88305c9d6edb87b46f
zerox13 <abdulsalam.aldahir@gmail.com>
2020-09-24 23:43:51 +0200

    Added zsh highlighting

--- .zshrc
+++ .zshrc
@@ -1,4 +1,17 @@
+autoload -U colors && colors
 PROMPT="%(?.%F{green}√.%F{red}?) %F{red}%!%B %F{yellow}Abbe%F{red}@%F{blue}%m %b%F{white}[%3~] %F{red}%#-->%F{yellow} "
+
+
+#Make the path to zsh highlighting dynamic
+zshHighlight=""
+if [[ `which brew` == *brew ]]
+then
+    zshHighlight="/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+else
+    #TODO: Change this to debian/Linux path
+    zshHighlight="/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+fi
+
 
 #-----< Update the alias >------
 if [ -f ~/.aliases ]; then
@@ -15,3 +28,4 @@ fi
 if [ -f ~/.paths ]; then
 . ~/.paths
 fi
+source $zshHighlight 2> /dev/null
--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 ln -f ~/dotfiles/.vimrc ~/.vimrc
 ln -f ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -f ~/dotfiles/.bashrc ~/.bashrc
-ln -f ~/dotfiles/.zshrc ~/.zshrc
 
 ln -f ~/dotfiles/.functions ~/.functions
 ln -f ~/dotfiles/.paths ~/.paths
@@ -11,8 +10,15 @@ ln -f ~/dotfiles/.aliases ~/.aliases
 #Checks if i3 installed, to link i3 config
 if [[ `which i3` == *i3 ]]
 then
-	ln -f ~/dotfiles/i3/config ~/.config/i3
-	ln -f ~/dotfiles/i3/i3blocks ~/.config/i3
-	echo "i3 is set up"
+    ln -f ~/dotfiles/i3/config ~/.config/i3
+    ln -f ~/dotfiles/i3/i3blocks ~/.config/i3
+    echo "i3 is set up"
+fi
+
+
+if [[ `which zsh` == *zsh ]]
+then
+    ln -f ~/dotfiles/.zshrc ~/.zshrc
+    echo "zsh is set up"
 fi
 
