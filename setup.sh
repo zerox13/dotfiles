310dea162ed08a02388c4bbcb7da2c90f2a280e4
zerox13 <abdulsalam.aldahir@gmail.com>
2021-02-03 12:51:59 +0100

    Imporved the setUp script, no more if statements haha

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Auther: Abdulsalam Aldahir
+
+
 printHelp(){
 	echo "Usage:"
 	echo -e "\t./setup <option>"
@@ -14,11 +17,11 @@ printHelp(){
 
 setupVim () {
 	echo "Setting vim up ..."
-	if [ ! -d "../.vim/bundle/Vundle.vim/" ]; then
-		git clone https://github.com/VundleVim/Vundle.vim.git ../.vim/bundle/Vundle.vim
-		vim +PluginInstall +qall
-		echo cloned Vundle, and plugins should be installed, You might need to run PluginInstall in Vim 
-	fi
+	[ ! -d "../.vim/bundle/Vundle.vim/" ] &&
+	git clone https://github.com/VundleVim/Vundle.vim.git ../.vim/bundle/Vundle.vim &&
+	vim +PluginInstall +qall &&
+	echo cloned Vundle, and plugins should be installed, You might need to run PluginInstall in Vim 
+		
 	# Linking the vimrc file
 	ln -f ~/dotfiles/.vimrc ~/.vimrc
 
@@ -26,12 +29,10 @@ setupVim () {
 
 setupI3(){
 	echo "Setting i3 up ..."
-	if [[ `which i3` == *i3 ]]
-	then
-		ln -f ~/dotfiles/i3/config ~/.config/i3
-		ln -f ~/dotfiles/i3/i3blocks ~/.config/i3
-		echo "i3 is set up"
-	fi
+	which i3 1> /dev/null && 
+	ln -f ~/dotfiles/i3/config ~/.config/i3 &&
+	ln -f ~/dotfiles/i3/i3blocks ~/.config/i3 &&
+	echo "i3 is set up"
 }
 
 setupVscode(){
@@ -54,10 +55,9 @@ generalSetup(){
 	ln -f ~/dotfiles/.aliases ~/.aliases
 	ln -f ~/dotfiles/.ghci ~/.ghci
 
-	if [[ `which zsh` == *zsh ]]; then
-		ln -f ~/dotfiles/.zshrc ~/.zshrc
-		echo "zsh is set up"
-	fi
+	which zsh 1> /dev/null && 
+	ln -f ~/dotfiles/.zshrc ~/.zshrc &&
+	echo "zsh is set up"
 }
 
  #setupVim
