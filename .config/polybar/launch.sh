89754f320c70cb696cedd6b361c0487f0edd2918
Abdulsalam Aldahir <abdulsalam.aldahir@gmail.com>
2021-02-18 13:04:29 +0100

    polybar config, made it as i want it

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,16 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+
+./.screenlayout/thinkV.sh
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
 
 echo "Bars launched..."
