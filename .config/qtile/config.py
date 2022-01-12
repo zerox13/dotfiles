2123460c2b0b74efbd66db9d864a4f2088e6f0f7
Abdulsalam <abdulsalam.aldahir@gmail.com>
2022-01-12 13:45:18 +0100

    Added nvim coc config and qtile

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -44,8 +44,8 @@ colors = [["#282c34", "#282c34"], # panel background
           ["#3d3f4b", "#434758"], # background for current screen tab
           ["#ffffff", "#ffffff"], # font color for group names
           ["#ff5555", "#ff5555"], # border line color for current tab
-          ["#74438f", "#74438f"], # border line color for 'other tabs' and color for 'odd widgets'
-          ["#4f76c7", "#4f76c7"], # color for the 'even widgets'
+          ["#55DD82", "#34eb7a"], # border line color for 'other tabs' and color for 'odd widgets'
+          ["#2BD463", "#2BD463"], # color for the 'even widgets'
           ["#e1acff", "#e1acff"], # window name
           ["#ecbbfb", "#ecbbfb"]] # backbround for inactive screens
 
