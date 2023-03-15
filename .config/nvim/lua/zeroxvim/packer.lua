8ad74e677508e7cbed354f7dda8c326b58d141d1
zerox13 <abdulsalam.aldahir@gmail.com>
2023-03-15 16:17:50 +0100

    More nvim stuff

--- .config/nvim/lua/zeroxvim/packer.lua
+++ .config/nvim/lua/zeroxvim/packer.lua
@@ -13,13 +13,7 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
-	use { 
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd.colorscheme('rose-pine')
-		end
-	}
+	use 'rose-pine/neovim'
 
 	use {'ellisonleao/gruvbox.nvim'}
 
@@ -63,5 +57,6 @@ return require('packer').startup(function(use)
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
         },
     }
+    use 'udalov/kotlin-vim'
 
 end)
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -119,6 +119,11 @@ _G.packer_plugins = {
     path = "/home/abbe/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/theprimeagen/harpoon"
   },
+  ["kotlin-vim"] = {
+    loaded = true,
+    path = "/home/abbe/.local/share/nvim/site/pack/packer/start/kotlin-vim",
+    url = "https://github.com/udalov/kotlin-vim"
+  },
   ["lsp-zero.nvim"] = {
     loaded = true,
     path = "/home/abbe/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
@@ -138,6 +143,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/abbe/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
+  },
+  neovim = {
+    loaded = true,
+    path = "/home/abbe/.local/share/nvim/site/pack/packer/start/neovim",
+    url = "https://github.com/rose-pine/neovim"
   },
   ["nvim-cmp"] = {
     loaded = true,
@@ -174,12 +184,6 @@ _G.packer_plugins = {
     path = "/home/abbe/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
-  ["rose-pine"] = {
-    config = { "\27LJ\2\n=\0\0\3\0\4\0\0066\0\0\0009\0\1\0009\0\2\0'\2\3\0B\0\2\1K\0\1\0\14rose-pine\16colorscheme\bcmd\bvim\0" },
-    loaded = true,
-    path = "/home/abbe/.local/share/nvim/site/pack/packer/start/rose-pine",
-    url = "https://github.com/rose-pine/neovim"
-  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/home/abbe/.local/share/nvim/site/pack/packer/start/telescope.nvim",
@@ -193,10 +197,6 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: rose-pine
-time([[Config for rose-pine]], true)
-try_loadstring("\27LJ\2\n=\0\0\3\0\4\0\0066\0\0\0009\0\1\0009\0\2\0'\2\3\0B\0\2\1K\0\1\0\14rose-pine\16colorscheme\bcmd\bvim\0", "config", "rose-pine")
-time([[Config for rose-pine]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
